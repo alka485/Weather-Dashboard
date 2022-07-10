ee94591ca41c642d6e2fbb8438af917d15778759
Alka sah <alkasah485@gmail.com>
2022-07-10 01:23:52 -0700

    event listener in java script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
+//var APIKey = b9f6c58391da8f005bd41c6735238193;
 var citySearchEl = document.getElementById("cityBox");
+var searchButton = document.getElementById("search");
+
+searchButton.addEventListener('click' , function(){
+    console.log("search");
+})
