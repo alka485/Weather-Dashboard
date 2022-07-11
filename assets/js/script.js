2f9cea7e098c56b1769f708512e438ea85b22097
Alka sah <alkasah485@gmail.com>
2022-07-11 08:03:58 -0700

    modifications saved

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
-
-
 console.log("hello");
 var citySearchEl = document.getElementById("cityBox");
 var searchButtonEl = document.getElementById ("search");
-var cityName = citySearchEl.value;
-console.log(citySearchEl);
-console.log(cityName);
-
-function getApi(){
-    //var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
+var displayCityEl = document.getElementById("city");
+function getApi(city){
+    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=b9f6c58391da8f005bd41c6735238193";
+    console.log(city);
+    console.log(requestUrl);
+    displayCityEl.value=requestUrl.html_main_name;
+    console.log(displayCityEl);
+    fetch(requestUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+    })
 }
 
 searchButtonEl.addEventListener("click",function(){
     console.log("search");
+    var cityName = citySearchEl.value;
+    console.log(cityName);
+    getApi(cityName);
 })
