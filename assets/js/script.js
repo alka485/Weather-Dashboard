8d71d74cabe31b5a6a64d478065f73d4f5acf5fc
Alka sah <alkasah485@gmail.com>
2022-07-12 01:42:13 -0700

    adding data through script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-console.log("hello");
+//console.log("hello");
 var citySearchEl = document.getElementById("cityBox");
 var searchButtonEl = document.getElementById ("search");
 var displayCityEl = document.getElementById("city");
+var displayTempEl = document.getElementById("temp");
+var displayHumidEl = document.getElementById("humidity");
 function getApi(city){
     var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=b9f6c58391da8f005bd41c6735238193";
     console.log(city);
     console.log(requestUrl);
+    //console.log(requestUrl_name);
     //displayCityEl.innerHTML=requestUrl_main;
     //console.log(displayCityEl);
     fetch(requestUrl)
@@ -14,12 +17,18 @@ function getApi(city){
     })
     .then(function(data){
         console.log(data)
+        console.log(data.name);
+    displayCityEl.innerHTML=data.name;
+    console.log(data.main.temp);
+    displayTempEl.innerHTML=data.main.temp;
+    displayHumidEl.innerHTML=data.main.humidity;
+
     })
 }
 
 searchButtonEl.addEventListener("click",function(){
-    console.log("search");
+    //console.log("search");
     var cityName = citySearchEl.value;
-    console.log(cityName);
+    //console.log(cityName);
     getApi(cityName);
 })
