17d6a891c6c78c9250a37bab79522194bac10df6
Alka sah <alkasah485@gmail.com>
2022-07-12 02:28:37 -0700

    added from bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,26 +9,16 @@ var displayDescEl = document.getElementById("description");
 var displayIconEl = document.getElementById("icon");
 function getApi(city){
     var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=b9f6c58391da8f005bd41c6735238193";
-    console.log(city);
-    console.log(requestUrl);
-    //console.log(requestUrl_name);
-    //displayCityEl.innerHTML=requestUrl_main;
-    //console.log(displayCityEl);
     fetch(requestUrl)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        console.log(data)
-        console.log(data.name);
     displayCityEl.innerHTML=data.name;
-    displayDescEl.innerHTML=data.weather[0].description;
-    //displayIconEl.innerHTML=data.weather[0].icon;
-    console.log(data.main.temp);
-    displayTempEl.innerHTML=data.main.temp;
-    displayHumidEl.innerHTML=data.main.humidity;
-    displayWindEl.innerHTML=data.wind.speed;
-
+    displayDescEl.innerHTML= "Description : " + data.weather[0].description;
+    displayTempEl.innerHTML= "Temperature : " + data.main.temp;
+    displayHumidEl.innerHTML= "Humidity   : " + data.main.humidity;
+    displayWindEl.innerHTML= "Wind_Speed  : " + data.wind.speed;
     })
 }
 
