8a357cce95e81d7ec9bb7c6629bd4b13ece562d9
Alka sah <alkasah485@gmail.com>
2022-07-12 01:56:15 -0700

    adding code in script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ var searchButtonEl = document.getElementById ("search");
 var displayCityEl = document.getElementById("city");
 var displayTempEl = document.getElementById("temp");
 var displayHumidEl = document.getElementById("humidity");
+var displayWindEl = document.getElementById("speed");
+var displayDescEl = document.getElementById("description");
+var displayIconEl = document.getElementById("icon");
 function getApi(city){
     var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=b9f6c58391da8f005bd41c6735238193";
     console.log(city);
@@ -19,9 +22,12 @@ function getApi(city){
         console.log(data)
         console.log(data.name);
     displayCityEl.innerHTML=data.name;
+    displayDescEl.innerHTML=data.weather[0].description;
+    //displayIconEl.innerHTML=data.weather[0].icon;
     console.log(data.main.temp);
     displayTempEl.innerHTML=data.main.temp;
     displayHumidEl.innerHTML=data.main.humidity;
+    displayWindEl.innerHTML=data.wind.speed;
 
     })
 }
